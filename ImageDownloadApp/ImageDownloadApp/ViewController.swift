17a4d97fb2335f3e1ca216884e8e67157757eaec
wndms710 <wndms710@naver.com>
2023-02-20 16:31:56 +0900

    20220220 프로젝트 생성

---
+++ ImageDownloadApp/ImageDownloadApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageDownloadApp
+//
+//  Created by 조주은 on 2023/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
